4cf21c7694cb96d9ff94b3a0fe19d2e21f80a7cf
Kamil Dudek <dudek.kamil17@gmail.com>
2021-04-15 17:56:32 +0200

    feature: add authorization method and test

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Response
+import hashlib
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", status_code=200)
 def root():
     return {"message": "Hello World"}
+
+
+@app.get("/method", status_code=200)
+def check_method():
+    return {"method": "GET"}
+
+
+@app.get("/auth", response_class=Response, status_code=204)
+def auth_method(password: str, password_hash: str):
+    m = hashlib.sha512()
+    m.update(str.encode(password))
+    if m.hexdigest() != password_hash:
+        raise HTTPException(status_code=401)
--- tests/tests_first_lecture.py
+++ tests/tests_first_lecture.py
@@ -1,4 +1,5 @@
 from fastapi.testclient import TestClient
+import pytest
 
 from ..main import app
 
@@ -9,3 +10,22 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+
+def test_post_method():
+    response = client.get(f"/method")
+    assert response.status_code == 200
+    assert response.json() == {"method": "GET"}
+
+
+@pytest.mark.parametrize("password, password_hash, status_code",
+                         [('haslo',
+                           ('013c6889f799cd986a735118e1888727d1435f7f623d05d58'
+                            'c61bf2cd8b49ac90105e5786ceaabd62bbc27336153d0d316'
+                            'b2d13b36804080c44aa6198c533215'),
+                           204), ('', '', 401),
+                          ('haslo', 'asdasda', 401)])
+def test_auth_method(password, password_hash, status_code):
+    response = client.get(
+        f"/auth?password={password}&password_hash={password_hash}")
+    assert response.status_code == status_code
