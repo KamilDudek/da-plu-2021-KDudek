211eaa59492eba06b47386a158d5b36c46de374f
Kamil Dudek <dudek.kamil17@gmail.com>
2021-04-17 14:36:35 +0200

    feature: add tests and improve register method

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, HTTPException, Response, Request
 from datetime import datetime, timedelta
+from fastapi.encoders import jsonable_encoder
 
+from pydantic import BaseModel
 import hashlib
 
 app = FastAPI()
@@ -8,8 +10,13 @@ app.counter = 0
 app.patients_table = []
 
 
+class Patient(BaseModel):
+    name: str
+    surname: str
+
+
 @app.get("/", status_code=200)
-def root():
+async def root():
     return {"message": "Hello World"}
 
 
@@ -26,30 +33,37 @@ async def auth_method(password: str, password_hash: str):
         raise HTTPException(status_code=401)
 
 
-@app.post('/register')
-async def register_post(request: Request):
-    register_data = await request.json()
+@app.post('/register', status_code=201)
+async def register_post(patient: Patient):
+    register_data = jsonable_encoder(patient)
     name_len = len(register_data['name'])
     surname_len = len(register_data['surname'])
+
+    if name_len == 0 or surname_len == 0:
+        raise HTTPException(status_code=422)
     app.counter += 1
     register_data['id'] = app.counter
     today = datetime.now()
-    register_data['date'] = today.strftime("%Y-%m-%d")
-    x = today + timedelta(days=surname_len + name_len)
-    register_data['vaccination_date'] = x.strftime("%Y-%m-%d")
-    z = list(register_data.items())
+    register_data['register_date'] = today.strftime("%Y-%m-%d")
+    vaccination_date = today + timedelta(days=surname_len + name_len)
+    register_data['vaccination_date'] = vaccination_date.strftime("%Y-%m-%d")
+    register_data_list = list(register_data.items())
     order = [2, 0, 1, 3, 4]
-    z = [z[i] for i in order]
-    register_data = dict(z)
-
+    register_data_list = [register_data_list[i] for i in order]
+    register_data = dict(register_data_list)
     app.patients_table.append(register_data)
 
     return register_data
 
 
-@app.get('/patient/{id}')
+@app.get('/patient/{id}', status_code=200)
 async def get_patient_by_id(id: int):
+    if id < 1:
+        raise HTTPException(status_code=400)
+
     for patient_dict in app.patients_table:
         for key, value in patient_dict.items():
             if key == 'id' and value == id:
                 return patient_dict
+
+    raise HTTPException(status_code=404)
--- tests/tests_first_lecture.py
+++ tests/tests_first_lecture.py
@@ -1,6 +1,6 @@
 from fastapi.testclient import TestClient
 import pytest
-
+from datetime import datetime, timedelta
 from ..main import app
 
 client = TestClient(app)
@@ -28,4 +28,32 @@ def test_post_method():
 def test_auth_method(password, password_hash, status_code):
     response = client.get(
         f"/auth?password={password}&password_hash={password_hash}")
+    assert response.status_code == status_code
+
+
+def test_register():
+    params = {
+        "name": "Jan",
+        "surname": "Nowak"
+    }
+    response = client.post(f"/register", json=params)
+    assert response.status_code == 201
+    print(response.json())
+    today = datetime.now()
+    name_len = len(params['name'])
+    surname_len = len(params['surname'])
+    vaccination_date = today + timedelta(days=surname_len + name_len)
+    assert response.json() == {
+        "id": 1,
+        "name": "Jan",
+        "surname": "Nowak",
+        "register_date": f"{today.strftime('%Y-%m-%d')}",
+        "vaccination_date": f"{vaccination_date.strftime('%Y-%m-%d')}"
+    }
+
+
+@pytest.mark.parametrize("id, status_code",
+                         [('1', 200), ('-1', 400), ('921834981274', 404)])
+def test_get_patient_by_id(id, status_code):
+    response = client.get(f"/patient/{id}")
     assert response.status_code == status_code
