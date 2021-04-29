e10604cf3cd3fd9f910b1660e2994a20af1ae50a
Kamil Dudek <dudek.kamil17@gmail.com>
2021-04-29 20:32:52 +0200

    feature: use basic auth

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-from fastapi import FastAPI, Request, Response, HTTPException, Cookie
+from fastapi import FastAPI, Request, Response, HTTPException, Cookie, Depends, \
+    status
 import base64
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from typing import Optional
 from fastapi.responses import HTMLResponse
 from datetime import date
 from fastapi.templating import Jinja2Templates
+import secrets
 
 app = FastAPI()
-app.access_tokens = []
+security = HTTPBasic()
+
 templates = Jinja2Templates(directory="templates")
+
+app.access_tokens = ''
 
 
 @app.get("/hello", response_class=HTMLResponse)
@@ -19,20 +25,27 @@ def index_static(request: Request):
 
 
 @app.post("/login_session")
-def login(response: Response, user: Optional[str] = None,
-          password: Optional[str] = None):
-    if user == 'a' and password == 'a':
+def login(response: Response,
+          credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, "4dm1n")
+    correct_password = secrets.compare_digest(credentials.password,
+                                              "NotSoSecurePa$$")
 
-        session_token_uncode = f'{user}{password}dfsds'
-        session_token_bytes = session_token_uncode.encode('ascii')
+    if not (correct_username or correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    else:
+        session_token_decode = f'{credentials.username}{credentials.password}dfsds'
+        session_token_bytes = session_token_decode.encode('ascii')
         base64_bytes = base64.b64encode(session_token_bytes)
         session_token = base64_bytes.decode('ascii')
-        app.access_tokens.append(session_token)
+        app.access_tokens = session_token
         response.set_cookie(key="session_token",
                             value=session_token)
         return {'token': session_token}
-    else:
-        raise HTTPException(status_code=401)
 
 
 @app.post("/login_token")
