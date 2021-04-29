442bcd04bc25a1ff3b093cd775688e87355a19af
Kamil Dudek <dudek.kamil17@gmail.com>
2021-04-29 18:39:58 +0200

    feature: add basic auth method

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response, HTTPException, Cookie
+from hashlib import sha512
 from fastapi.responses import HTMLResponse
 from datetime import date
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.access_tokens = []
 templates = Jinja2Templates(directory="templates")
 
 
@@ -13,3 +15,24 @@ def index_static(request: Request):
     today_good_form = today.strftime("%Y-%m-%d")
     return templates.TemplateResponse("today.html", {
         "request": request, "today": today_good_form})
+
+
+@app.post("/login_session")
+def login(user: str, password: str, response: Response):
+    if user == '4dm1n' and password == 'NotSoSecurePa$$':
+
+        session_token = sha512(f'{user}{password}12312'.encode()).hexdigest()
+        app.access_tokens.append(session_token)
+        response.set_cookie(key="session_token",
+                            value=session_token)
+        return session_token
+    else:
+        raise HTTPException(status_code=401)
+
+
+@app.post("/login_token")
+def create_cookie(*, response: Response, session_token: str = Cookie(None)):
+    if session_token not in app.access_tokens:
+        raise HTTPException(status_code=401)
+    else:
+        return {'token': session_token}
