f0b3533bfe1eb342c8cfed5e5fcb32a67c174d6c
Dudek <kamil.dudek@nokia.com>
2021-05-10 17:11:40 +0200

    featur: add 4.1 to 4.5 exercises;

--- main.py
+++ main.py
@@ -132,6 +132,22 @@ async def root():
 
 
 # task1
+@app.get("/categories", status_code=200)
+async def categores():
+    app.db_connection.row_factory = sqlite3.Row
+    data = app.db_connection.execute('''
+    SELECT CategoryID, CategoryName FROM Categories ORDER BY CategoryID
+    ''').fetchall()
+    return {"categories": [{"id": x['CategoryID'], "name": x["CategoryName"]} for x in data]}
+
+@app.get("/customers", status_code=200)
+async def customers():
+    app.db_connection.row_factory = sqlite3.Row
+    data = app.db_connection.execute('''
+    SELECT CustomerID, CompanyName, (COALESCE(Address, '') || ' ' || COALESCE(PostalCode, '') || ' ' || COALESCE(City, '') || ' ' || COALESCE(Country, '')) AS full_address FROM Customers ORDER BY CustomerID
+    ''').fetchall()
+    return {"customers": [{"id": f"{x['CustomerID']}", "name": x["CompanyName"], "full_address": (x["full_address"])} for x in data]}
+
 # taks2
 @app.get("/products/{id}")
 async def products_by_id(id: int):
@@ -143,3 +159,36 @@ async def products_by_id(id: int):
         return product
     else:
         raise HTTPException(status_code=404, detail="ID doesn't exist")
+
+#task3
+@app.get('/employees', status_code=200)
+async def employees(limit: int = -1, offset: int = 0, order: str = 'id'):
+    app.db_connection.row_factory = sqlite3.Row
+    columns = {'first_name' : 'FirstName', 'last_name' : 'LastName', 'city' : 'City', 'id' : 'EmployeeID'}
+    if order not in columns.keys():
+        raise HTTPException(status_code=400)
+    order = columns[order]
+    data = app.db_connection.execute(f"SELECT EmployeeID, LastName, FirstName, City FROM Employees ORDER BY {order} LIMIT ? OFFSET ?",(limit, offset, )).fetchall()
+    return {"employees": [{"id": x['EmployeeID'],"last_name":x['LastName'],"first_name":x['FirstName'],"city":x['City']} for x in data]}
+
+#task4
+@app.get('/products_extended', status_code=200)
+async def products_extended():
+    app.db_connection.row_factory = sqlite3.Row
+    data = app.db_connection.execute('''
+    SELECT Products.ProductID AS id, Products.ProductName AS name, Categories.CategoryName AS category, Suppliers.CompanyName AS supplier FROM Products 
+    JOIN Categories ON Products.CategoryID = Categories.CategoryID JOIN Suppliers ON Products.SupplierID = Suppliers.SupplierID ORDER BY Products.ProductID
+    ''').fetchall()
+    return {"products_extended": [{"id": x['id'], "name": x['name'], "category": x['category'], "supplier": x['supplier']} for x in data]}
+
+#task5
+@app.get('/products/{id}/orders', status_code=200)
+async def products_id_orders(id: int):
+    app.db_connection.row_factory = sqlite3.Row # (UnitPrice x Quantity) - (Discount x (UnitPrice x Quantity))
+    data = app.db_connection.execute(f'''
+    SELECT Products.ProductID, Orders.OrderID AS id, Customers.CompanyName AS customer, [Order Details].Quantity AS quantity, [Order Details].UnitPrice AS unitprice, [Order Details].Discount as discount 
+    FROM Products JOIN [Order Details] ON Products.ProductID = [Order Details].ProductID JOIN Orders ON [Order Details].OrderID = Orders.OrderID JOIN Customers ON Orders.CustomerID = Customers.CustomerID WHERE Products.ProductID = {id} ORDER BY Orders.OrderID
+    ''').fetchall()
+    if data == []:
+        raise HTTPException(status_code=404) #
+    return {"orders": [{"id": x["id"], "customer": x["customer"], "quantity": x["quantity"], "total_price": round(((x['unitprice'] * x['quantity']) - (x['discount'] * (x['unitprice'] * x['quantity']))), 2)} for x in data]}
