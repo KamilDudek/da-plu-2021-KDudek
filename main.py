fb80a2fb7888fdcb05324f65d30db27526cf73a1
Dudek <kamil.dudek@nokia.com>
2021-05-10 19:01:31 +0200

    feature: change task1

--- main.py
+++ main.py
@@ -133,36 +133,24 @@ async def root():
 
 # task1
 @app.get("/categories")
-async def categories():
-    app.db_connection.row_factory = sqlite3.Row
-    data = app.db_connection.execute(
-        "SELECT CategoryId AS id, CategoryName AS name FROM Categories").fetchall()
-    return {
-        'categories': data
-    }
+async def get_categories():
+    cursor = app.db_connection.cursor()
+    categories = cursor.execute("SELECT  CategoryID, CategoryName FROM Categories ORDER BY CategoryID").fetchall()
+    output = dict(categories=[dict(id=row[0], name=row[1]) for row in categories])
+    return output
 
 
 @app.get("/customers")
-async def customers():
-    app.db_connection.row_factory = sqlite3.Row
-    data = app.db_connection.execute(
-        "SELECT CustomerId, CompanyName, Address, PostalCode, City, Country FROM customers").fetchall()
-    refactored = []
-    for row in data:
-        keys = ['Address', 'PostalCode', 'City', 'Country']
-        full_address = []
-        for key in keys:
-            if row[key]:
-                full_address.append(row[key])
-
-        refactored.append(
-            {'id': row['CustomerId'],
-             'name': row['CompanyName'],
-             'full_address': ' '.join(full_address)}
-        )
-    return {
-        'customers': refactored
-    }
+async def get_customers():
+    cursor = app.db_connection.cursor()
+    cursor.row_factory = sqlite3.Row
+    customers = cursor.execute(
+        "SELECT CustomerID id, COALESCE(CompanyName, '') name, "
+        "COALESCE(Address, '') || ' ' || COALESCE(PostalCode, '') || ' ' || COALESCE(City, '') || ' ' || "
+        "COALESCE(Country, '') full_address "
+        "FROM Customers c ORDER BY UPPER(CustomerID);"
+    ).fetchall()
+    return dict(customers=customers)
 # taks2
 @app.get("/products/{id}")
 async def products_by_id(id: int):
